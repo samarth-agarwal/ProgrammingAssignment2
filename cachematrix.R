f4c8f9853e86d310e2193d6e2597d6de8d535a05
Samarth <samarthagarwal.100@gmail.com>
2014-12-09 16:04:30 +0530

    Second Commit

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,11 @@
 ## functions do
 
 ## Write a short comment describing this function
+## The makeCachMatrix performs 4 functions, namely:
+##  1. Set the Matrix x
+##  2. Retrieve (get) the matrix x
+##  3. Set the inverse of the matrix x (xinv), and store it in the cache memory
+##  4. Retrieve xinv from cache
 
 makeCacheMatrix <- function(x = matrix()) 
 {
@@ -21,6 +26,10 @@ makeCacheMatrix <- function(x = matrix())
 
 
 ## Write a short comment describing this function
+## This function is used to retirve the inverse of matrix x
+##  1. If the inverse is already saved in cache memory, it will retieve it.
+##  2. Else, it will calcute it within the function and then cache it.
+##  3. In the end, it will return in the inverse obtained.
 
 cacheSolve <- function(x, ...) 
 {
